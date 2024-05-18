979d34b3ae04a055a9910c40d02957af1d0b4f52
Kostya2898 <167633710+Kostya2898@users.noreply.github.com>
2024-05-19 00:38:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Player():
         self.width = width  
         self.height = height  
         self.lives = 3  
-        self.rotation_angle = 0  # Початковий кут нахилу
+        self.rotation_angle = 0
 
     def update_animation(self, velocity):
         self.index += 0.1  
@@ -22,11 +22,10 @@ class Player():
             self.index = 0
         self.image = self.frames[int(self.index)]
         
-        if velocity != 0:  # Перевірка, чи пташка летить
-            # Застосування нахилу пташки вперед залежно від швидкості
-            self.rotation_angle = min(max(-30, velocity * -2), 30)  # Кут нахилу пташки
+        if velocity != 0:
+            self.rotation_angle = min(max(-30, velocity * -2), 30)
         else:
-            self.rotation_angle = 0  # Якщо не летить, нахил = 0
+            self.rotation_angle = 0
 
         self.image = pygame.transform.rotate(self.image, self.rotation_angle)
 
@@ -71,7 +70,7 @@ bird_frames = [bird_image.subsurface(pygame.Rect(i * frame_width, 0, frame_width
 
 player = Player(50, 50, 50, 50, bird_frames)
 
-state = 'menu'  # Початковий стан - меню
+state = 'menu'
 timer = 10  
 pipes = []  
 play = True  
@@ -93,18 +92,22 @@ def create_pipe():
     return pipe_top_image, pipe_bottom_image, pipe_top_rect, pipe_bottom_rect  
 
 # Додавання музики
-pygame.mixer.music.load('music.mp3')  # Музика під час гри
-pygame.mixer.music.play(-1)  # Запуск музики з постійним повторенням
+pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play(-1)
 
 # Додавання звуку при втраті життя
 fall_sound = pygame.mixer.Sound('fall.wav')
 
-# Завантаження зображення шестеренки
-gear_image = pygame.image.load('download.png')
-scaled_gear_width = 40  # Нова ширина зображення
-scaled_gear_height = 40  # Нова висота зображення
-gear_image = pygame.transform.scale(gear_image, (scaled_gear_width, scaled_gear_height))
-gear_position = (10, 10)  # Координати зображення
+# Завантаження зображення шестерні та обрізання білого фону
+gear_image = None
+gear_position = (10, 10)
+try:
+    gear_image = pygame.image.load('download.png').convert_alpha()
+    gear_image = pygame.transform.scale(gear_image, (50, 50))  # Масштабування до 50x50
+    gear_image.set_colorkey((255, 255, 255))  # Робимо білий колір прозорим
+    gear_rect = gear_image.get_rect(topleft=gear_position)
+except pygame.error:
+    print("Файл 'download.png' не знайдено в робочій директорії.")
 
 # Функція для відображення кнопки
 def draw_button(text, position, font, color, bgcolor):
@@ -131,6 +134,12 @@ while play:
                     player.rect.y = py
                 if quit_button.collidepoint(mouse_pos):
                     play = False
+                if gear_rect.collidepoint(mouse_pos):
+                    state = 'settings'
+            elif state == 'settings':
+                mouse_pos = event.pos
+                if back_button.collidepoint(mouse_pos):
+                    state = 'menu'
 
     window.blit(background_image, (0, 0))  
     
@@ -141,9 +150,15 @@ while play:
         start_button = draw_button("Почати гру", (WIDTH // 2, HEIGHT // 2), button_font, (255, 255, 255), (0, 0, 0))
         quit_button = draw_button("Вийти", (WIDTH // 2, HEIGHT // 2 + 100), button_font, (255, 255, 255), (0, 0, 0))
         
-        # Зображення шестеренки
-        window.blit(gear_image, gear_position)
+        if gear_image:
+            window.blit(gear_image, gear_position)
 
+    elif state == 'settings':
+        title_font = pygame.font.Font(None, 74)
+        button_font = pygame.font.Font(None, 50)
+        display_text("Налаштування", (150, 10), title_font, (255, 255, 255))
+        back_button = draw_button("Назад", (WIDTH // 2, HEIGHT // 2 + 100), button_font, (255, 255, 255), (0, 0, 0))
+    
     else:
         for pipe_top, pipe_bottom, pipe_top_rect, pipe_bottom_rect in pipes:
             window.blit(pipe_top, (pipe_top_rect.x, pipe_top_rect.y))
@@ -151,7 +166,6 @@ while play:
 
         window.blit(player.image, (player.rect.x, player.rect.y))  
 
-        # Відображення кількості життів та очок
         display_text("Життя: " + str(player.lives), text_lives_position, font)  
         display_text("Очки: " + str(score), text_score_position, font)  
 
@@ -187,7 +201,7 @@ while play:
             if player.rect.top < 0 or player.rect.bottom > HEIGHT:
                 sy, ay = 0, 0
                 player.lives -= 1
-                fall_sound.play()  # Відтворення звуку при втраті життя
+                fall_sound.play()
                 if player.lives == 0:
                     state = 'menu'
                 else:
@@ -196,7 +210,7 @@ while play:
             for pipe_top, pipe_bottom, pipe_top_rect, pipe_bottom_rect in pipes:
                 if player.rect.colliderect(pipe_top_rect) or player.rect.colliderect(pipe_bottom_rect):
                     player.lives -= 1
-                    fall_sound.play()  # Відтворення звуку при втраті життя
+                    fall_sound.play()
                     if player.lives == 0:
                         state = 'menu'
                     else:
@@ -208,17 +222,15 @@ while play:
 
         if state == 'play':
             for pipe_top, pipe_bottom, pipe_top_rect, pipe_bottom_rect in pipes:
-                if pipe_top_rect.right < player.rect.x and pipe_top_rect.right > 0 and pipe_top_rect.right < WIDTH // 2 and pipe_top_rect.right > player.rect.x - 3:
+                 if pipe_top_rect.right < player.rect.x and pipe_top_rect.right > 0 and pipe_top_rect.right < WIDTH // 2 and pipe_top_rect.right > player.rect.x - 3:
                     score += 10
 
         if state != 'start':
             pipes = [pipe_pair for pipe_pair in pipes if pipe_pair[2].right > 0]
 
-        player.update_animation(sy)  # Передача швидкості польоту для визначення нахилу пташки
+        player.update_animation(sy)
 
     pygame.display.update()  
     clock.tick(FPS)
 
 pygame.quit()
-
-
